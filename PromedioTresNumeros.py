f116f276f997a52822fc1b5e7ed248741cdc71d9
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-07-22 21:39:23 -0500

    calses 22

---
+++ PromedioTresNumeros.py
@@ -0,0 +1,11 @@
+#Escriba un programa que solicite al usuario ingrese tres numeros
+#para luego mostrarle el promedio de los tres
+
+n1 = int(input("ingrese n1 "))
+
+n2 = int(input("ingrese n2 "))
+
+n3 =int(input("ingrese n3 "))
+suma = n1 + n2 + n3
+p = suma / 3
+print(p)
