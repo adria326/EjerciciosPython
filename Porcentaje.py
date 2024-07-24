cce75ef9646a04c5fb4d7fc7693319aeb865d745
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-07-23 19:45:35 -0500

    clases 2

---
+++ Porcentaje.py
@@ -0,0 +1,11 @@
+#Escribe un programa que solicite al usuario
+#un numero y le reste el 15%, almacenando
+#todo en una unica variable. A continuacion,
+#mostar el resultado final en pantalla.
+
+n = int(input("ingreseun numero: "))
+p = (n * 15)/100
+r = n - p
+print(r)
+
+
