2488871c45aeb42987af962ba37a962d7567a7ba
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-07-23 20:43:26 -0500

    palabra mas espacio

---
+++ concatenar.py
@@ -0,0 +1,9 @@
+#Escriba un programa que solicite al usuario el ingreso de dos palabras,
+#las cuales se guardan en dos variables distintas.
+#A continuacion, almacena en una variable la concatenacion de la primera palabra,
+#mas un espacio, mas la segunda palabra. Muestre el resultado en pantalla.
+
+p1 = input("ingrese la primera palabra: ")
+p2 = input("ingrese la segunda palabra: ")
+p3 = p1 +" "+p2
+print(p3)
