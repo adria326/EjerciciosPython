b62e4903be5f56876a8e726cd96d09c74138846e
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-08-03 10:59:41 -0500

    ArreglosUnidimensionales

---
+++ valor.py
@@ -0,0 +1,20 @@
+numeros = [1]
+enteros = numeros
+enteros.append(2)
+print(numeros)
+print(enteros)
+
+#copia individuales
+numrosindividuales=[1]
+enterosindividuales=numerosindividuales[:]
+enterosindividuales.append(2)
+print(numerosinviduales)
+print(enterosindividuales)
+
+# n=1
+# x=n
+# x=2
+# print(n,x)
+
+# lista=[1, 2, 3]
+# print("lista original", lista)
